a36d633140a9279e215f314631f1c135e429cb26
Tyler <41713505+Tylersuard@users.noreply.github.com>
2022-04-04 13:59:09 -0700

    Create Example1.py

---
+++ Example1.py
@@ -0,0 +1,8 @@
+def foo(array):
+    sum = 0
+    product = 1
+    for i in range(len(array)):
+        sum += array[i]
+    for i in range(len(array)):
+        product *= array[i]
+    print(sum, product)
