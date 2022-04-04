04a138a68ad6321953e997c30cd367e998a684d8
Tyler <41713505+Tylersuard@users.noreply.github.com>
2022-04-04 14:07:46 -0700

    Create Example4.py

---
+++ Example4.py
@@ -0,0 +1,5 @@
+def printUnorderedPairs(arrayA, arrayB):
+    for i in range(len(arrayA)):
+        for j in range(len(arrayB)):
+            if arrayA[i] < arrayB[j]:
+                print(arrayA[i] + ", " + arrayB[j])  
