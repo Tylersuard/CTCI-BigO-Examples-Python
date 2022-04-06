cb34312c2ee551ea0991d94b09461d134c1998fe
Tyler <41713505+Tylersuard@users.noreply.github.com>
2022-04-06 16:15:19 -0700

    Create Example12.py

---
+++ Example12.py
@@ -0,0 +1,7 @@
+def permutation(str,prefix=""):
+    if(len(str)==0):
+        print(prefix)
+    else:
+        for i in range(len(str)):
+            rem = str[0:i] + str[i+1:]
+            permutation(rem, prefix + str[i])
